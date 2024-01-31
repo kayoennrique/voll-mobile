da2853eb8a015d2de3c329eecdaf00d669e1c709
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-31 00:21:05 -0300

    style(git): finishing the login screen

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,12 @@
-import { Image, Text, Box, FormControl, VStack, Input } from 'native-base';
+import { VStack, Image, Text, Box, FormControl, Input, Button, Link } from 'native-base';
+import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png';
 
 export default function Login() {
   return (
-    <VStack flex={1} alignItems="center" p={5}>
+    <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
       <Image source={Logo} alt="Logo Voll" />
+
       <Text
         fontSize="2xl"
         fontWeight="bold"
@@ -20,25 +22,44 @@ export default function Login() {
           <Input
             placeholder='Insira seu endereço de email'
             size='lg'
-            w='100%'
+            w="100%"
+            borderRadius='lg'
+            bgColor='gray.100'
+            shadow={3}
+          />
+        </FormControl>
+        <FormControl mt={3}>
+          <FormControl.Label>Senha</FormControl.Label>
+          <Input
+            placeholder='Insira sua senha'
+            size='lg'
+            w="100%"
             borderRadius='lg'
             bgColor='gray.100'
             shadow={3}
           />
         </FormControl>
       </Box>
-      <Box>
-        <FormControl mt={3}>
-          <FormControl.Label>Senha</FormControl.Label>
-          <Input
-            placeholder='Insira sua senha'
-            size='lg'
-            w='100%'
-            borderRadius='lg'
-            bgColor='gray.100'
-            shadow={3}
-          />
-        </FormControl>
+      <Button
+        w="100%"
+        bg="blue.800"
+        mt={10}
+        borderRadius="lg"
+      >
+        Entrar
+      </Button>
+
+      <Link href='https://www.alura.com.br'>
+        Esqueceu sua senha?
+      </Link>
+
+      <Box w="100%" flexDirection="row" justifyContent="center">
+        <Text>Ainda não tem cadastro? </Text>
+        <TouchableOpacity>
+          <Text color="blue.500">
+            Faça seu cadastro!
+          </Text>
+        </TouchableOpacity>
       </Box>
     </VStack>
   );
