008a1de555e091b2eb340dcdfba0e86d21617a2d
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-01 17:59:00 -0300

    style(git): styling the main screen

--- src/Tabs/Main.tsx
+++ src/Tabs/Main.tsx
@@ -1,9 +1,44 @@
-import { VStack, Text } from 'native-base'
+import { VStack, Image, Box, ScrollView, Text, Divider } from 'native-base';
+import Logo from '../assets/Logo.png';
+import { Bud } from "../components/Button";
+import { InputText } from "../components/InputText";
+import { Title } from "../components/Title";
+import { depositions } from "../utils/mock";
 
 export default function Main() {
+
   return (
-    <VStack>
-      <Text> Main</Text>
-    </VStack>
-  )
+    <ScrollView flex={1} bgColor="white">
+      <VStack flex={1} alignItems="flex-start" justifyContent="flex-start" p={5}>
+        <Image source={Logo} alt="Logo" mt={10} />
+        <Title color="blue.500">Boas-vindas!</Title>
+
+        <Box w="100%" borderRadius="lg" p={3} mt={10} shadow="1" borderRightRadius="md">
+          <InputText
+            placeholder="Digite a especialidade"
+          />
+          <InputText
+            placeholder="Digite sua localização"
+          />
+          <Bud mt={3} mb={3}>
+            Buscar
+          </Bud>
+        </Box>
+
+        <Title color="blue.800" alignSelf="center">Depoimentos</Title>
+        <VStack space={3} divider={<Divider />} w="100%">
+          {
+            depositions.map(deposition => (
+              <Box key={deposition.id} w="100%" borderRadius="lg" p={3}>
+                <Text color="gray.300" fontSize="md" textAlign="justify">
+                  {deposition.text}
+                </Text>
+                <Text color="gray.500" fontSize="lg" fontWeight="bold" alignSelf="center" mt="2">{deposition.title}</Text>
+              </Box>
+            ))
+          }
+        </VStack>
+      </VStack>
+    </ScrollView>
+  );
 }
---
+++ src/utils/mock.ts
@@ -0,0 +1,19 @@
+const depositions = [
+  {
+    id: 1,
+    title: 'Júlio, 31 anos, São Paulo/SP.',
+    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.',
+  },
+  {
+    id: 2,
+    title: 'Ricardo, 40 anos, Rio de Janeiro/RJ.',
+    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.',
+  },
+  {
+    id: 3,
+    title: 'Roberto, 53 anos, Salvador/BA.',
+    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.',
+  }
+]
+
+export { depositions }
