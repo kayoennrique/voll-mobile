9b9166100086547a741885d367aa939b02134ef4
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-01 17:49:28 -0300

    style(git): creating profile screen
    Styling the Profile screen

--- src/Tabs/Profile.tsx
+++ src/Tabs/Profile.tsx
@@ -1,10 +1,25 @@
-import { VStack, Text } from 'native-base'
+import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base';
+import { Title } from '../components/Title';
 
 export default function Profile() {
   return (
-    <VStack>
-      <Text> Profile</Text>
-    </VStack>
-  )
+    <ScrollView flex={1}>
+      <VStack flex={1} alignItems="center" p={5}>
+        <Title color="blue.500">Meu Perfil</Title>
+        <Avatar
+          size="xl"
+          source={{ uri: "https://github.com/kayoennrique.png" }}
+          mt={5}
+        />
+        <Title color="blue.500">Informações pessoais</Title>
+        <Title fontSize="lg" mb={1}>Kayo Ennrique</Title>
+        <Title>30/06</Title>
+        <Title>Rio de Janeiro - RJ</Title>
+        <Divider mt={5} />
+        <Title color="blue.500" mb={1}>Histórico médico</Title>
+        <Text>Sinusite</Text>
+        <Text>Rinite</Text>
+      </VStack>
+    </ScrollView>
+  );
 }
-
