f578079386e5c27f8fe720ae4c5bcdd647290092
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-02 02:25:45 -0300

    build(npm): login permanence
    Installing and applying AsyncStorage and JwtDecode

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,26 +1,54 @@
-import { VStack, Image, Text, Box, Link } from 'native-base';
+import { VStack, Image, Text, Box, Link, useToast } from 'native-base';
 import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png';
 import { Title } from './components/Title';
 import { InputText } from './components/InputText';
 import { Bud } from './components/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toDoLogin } from './services/AuthenticationService';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import jwtDecode from 'jwt-decode';
 
 export default function Login({ navigation }: any) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [loading, setLoading] = useState(true);
+  const toast = useToast();
+
+  useEffect(() => {
+    async function checkLogin() {
+      const token = await AsyncStorage.getItem('token')
+      if (token) {
+        navigation.replace('Tabs');
+      }
+      setLoading(false);
+    }
+    checkLogin();
+  }, [])
 
   async function login() {
     const result = await toDoLogin(email, senha);
     if (result) {
-      navigation.replace('Tabs');
+      const { token } = result;
+      AsyncStorage.setItem('token', token)
+
+      const decodeToken = jwtDecode(token) as any
+      const patientId = decodeToken.id
+      AsyncStorage.setItem('patientId', patientId)
+      navigation.replace('Tabs')
     }
     else {
-      console.log('Erro');
+      toast.show({
+        title: "Erro no login",
+        description: "O email ou senha não conferem",
+        backgroundColor: "red.500"
+      })
     }
   };
 
+  if (loading) {
+    return null
+  }
   return (
     <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
       <Image source={Logo} alt="Logo Voll" />
