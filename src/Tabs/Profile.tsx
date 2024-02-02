0aa1c910757a65e68e66a740b6c49316f0158f3b
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-02 16:18:48 -0300

    feat(git): consuming user data

--- src/Tabs/Profile.tsx
+++ src/Tabs/Profile.tsx
@@ -1,7 +1,26 @@
 import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base';
 import { Title } from '../components/Title';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getPatientData } from '../services/PatientService';
 
 export default function Profile() {
+  const [dataPatient, setDataPatient] = useState({});
+
+  useEffect(() => {
+    async function dataPatient() {
+      const patientId = await AsyncStorage.getItem('patientId');
+      if (!patientId) return null;
+
+      const result = await getPatientData(patientId)
+      if (result) {
+        setDataPatient(result)
+        console.log(result);
+      }
+    }
+    dataPatient()
+  }, []);
+
   return (
     <ScrollView flex={1}>
       <VStack flex={1} alignItems="center" p={5}>
@@ -12,7 +31,7 @@ export default function Profile() {
           mt={5}
         />
         <Title color="blue.500">Informações pessoais</Title>
-        <Title fontSize="lg" mb={1}>Kayo Ennrique</Title>
+        <Title fontSize="lg" mb={1}>{dataPatient.nome}</Title>
         <Title>30/06/1992</Title>
         <Title>Rio de Janeiro - RJ</Title>
         <Divider mt={5} />
--- src/services/PatientService.ts
+++ src/services/PatientService.ts
@@ -5,12 +5,23 @@ export async function registerPatient(patient: Patient) {
   if (!patient) return null;
 
   try {
-    const result = await api.post('/paciente', patient)
-    console.log(result.data)
-    return result.data
+    const result = await api.post('/paciente', patient);
+    console.log(result.data);
+    return result.data;
   }
   catch (error) {
-    console.log(error)
-    return null
+    console.log(error);
+    return null;
+  }
+}
+
+export async function getPatientData(id: string) {
+  try {
+    const result = await api.get(`/paciente/${id}`);
+    return result.data;
+  }
+  catch (error) {
+    console.log(error);
+    return null;
   }
 }
